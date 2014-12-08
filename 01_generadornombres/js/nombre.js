bc0c3212e99db74418044edd94b82521d3532dab
Edwin Torres <etorres.net@gmail.com>
2014-12-07 22:51:31 -0500

    Ajuste de estilos

--- 01_generadornombres/js/nombre.js
+++ 01_generadornombres/js/nombre.js
@@ -1,6 +1,6 @@
 var patron = null;
-var vocales = Array("a", "e", "i", "o", "u");
-var consonantes = Array("b", "c", "d","f", "g", "h", "j", "k","l","m","n","p","q","r","s","t","v","w","x","y","z");
+var vocales = "AEIOU";
+var consonantes = "BCDFGHJKLMNOPQRSTUVWXYZ";
 var cantidad_nombres = 0;
 
 $( document ).ready(function() {
@@ -12,24 +12,26 @@ $( document ).ready(function() {
 });
 
 function generar_nombres(){
-	patron = $("#patron").val();
+	patron = $("#patron").val().toLowerCase();
 	cantidad_nombres = $("#cantidad").val();
 
-	var html = "Patron : <strong>" + patron + "</strong>";
-	html += "<br/>Cantidad : <strong>" + cantidad_nombres + "</strong>";
+	var textoHtml = "<p style='margin-left: 20px'>Patron : <label>" + patron + "</label></p>";
+	textoHtml += "<p style='margin-left: 20px'>Cantidad : <label>" + cantidad_nombres + "</label></p>";
+	$("#entradas").html(textoHtml);
 
+	textoHtml = "";
 	for(i=0; i<cantidad_nombres; i++){
 		var nombre = "";
 		for(j=0; j<=patron.length; j++){
-			if(patron.charAt(j) == 'c'){
-				nombre += consonantes[Math.floor(Math.random() * consonantes.length)];				
-			}else if(patron.charAt(j) == 'v'){
-				nombre += vocales[Math.floor(Math.random() * vocales.length)];
+			if(patron.charAt(j).toLowerCase() == 'c'){
+				nombre += consonantes.charAt(Math.floor(Math.random() * consonantes.length));
+			}else if(patron.charAt(j).toLowerCase() == 'v'){
+				nombre += vocales.charAt(Math.floor(Math.random() * vocales.length));
 			}			
 		}
-		html += "<br> " + (i+1) + ". " + nombre;
+		textoHtml += "<tr><th> " + (i+1) + "</th><td>" + nombre + "</td></tr>";
 	}
 
-	$("#resultado").html(html);
+	$("#resultado table tbody").html(textoHtml);
 
 }
